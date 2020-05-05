871b8392e91d9f401ab491fd1bd19bc6ba05155c
azabraao <az.abraao@gmail.com>
2020-05-05 20:16:08 -0300

    Add first route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tarefas', function () {
+    $tarefas = [
+        "Estudar Lavarel",
+        "Fazer a NAC",
+        "Tirar 10 na PS",
+        "Dar like no canal do prof",
+        "Ir ao mercado",
+        "Comprar uma bike",
+        "Comprar um Mac"
+    ];
+
+    echo "<h1>Tarefas para fazer</h1>";
+
+    echo "<ul>";
+        foreach($tarefas as $tarefa) {
+            echo "<li> $tarefa </li>";
+        }
+    echo "</ul>";
 });
