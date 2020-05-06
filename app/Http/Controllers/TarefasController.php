afe88ffd3219dbc09afebd242043c091f386dc8c
azabraao <az.abraao@gmail.com>
2020-05-05 22:47:53 -0300

    Add store task on sqlite

--- app/Http/Controllers/TarefasController.php
+++ app/Http/Controllers/TarefasController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Tarefa;
+
 class TarefasController {
 
   public function index() {
-    $tarefas = [
-        "Estudar Lavarel",
-        "Fazer a NAC",
-        "Tirar 10 na PS",
-        "Dar like no canal do prof",
-        "Ir ao mercado",
-        "Comprar uma bike",
-        "Comprar um Mac"
-    ];
-    
+    $tarefas = Tarefa::all();
     return view('tarefas.index', compact('tarefas'));
+  }
+
+  public function store(Request $request) {
+    
+    $descricao = $request->tarefa;
+    $tarefa = new Tarefa();
+    $tarefa->descricao = $descricao;
+    $tarefa->feita = false;
+    var_dump($tarefa->save());
   }
 }
